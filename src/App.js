6532e6f89f2a11eaa9e6dcba96c25bad2081b93f
Anthony Casson <anthony.casson@gmail.com>
2020-09-03 11:41:44 -0700

    update app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hell from V2
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AmplifySignOut />
+      <h1>Welcome, you are signed in!</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default withAuthenticator(App);
